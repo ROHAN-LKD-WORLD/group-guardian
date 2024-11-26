6d915216e74fabb19f870b673be96501387bd0c3
LINK_KI_DUNIYA <158588158+ROHAN-LKD-WORLD@users.noreply.github.com>
2024-11-26 18:54:44 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7424793777:AAH-CFFgsO7u3axgl14VEtx5Z3mFuUTd3tA")
+API_ID = int(os.environ.get("API_ID", "9303922"))
+API_HASH = os.environ.get("API_HASH", "a17677495aa9ae010b897c2d65146282")
 SPOILER_MODE = bool(os.environ.get("SPOILER_MODE", True))
